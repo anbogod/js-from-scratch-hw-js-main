ef205e72199fc5ec950963797a530f0cacc4b5c5
anbogod <bogodukhov99@gmail.com>
2026-01-29 14:09:53 +0500

    complete lesson-01-02

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -6,4 +6,14 @@
 */
 
 // your code
-let factorial
+let factorial = 1;
+
+let add = 1;
+
+while (add < 10) {
+  factorial = factorial * (add + 1);
+
+  add = add + 1;
+}
+
+console.log(`Факториал =  ${factorial}`);
