df3e3fd70ac2b214781593bc07f10f11f24944e1
anbogod <bogodukhov99@gmail.com>
2026-01-29 15:23:02 +0500

    complete lesson-01-03

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -5,4 +5,19 @@
 */
 
 // your code
-let sum
+let sum = 0;
+
+let add = 1;
+
+while (add < 20) {
+  if (add % 2 === 1) {
+    
+    sum = sum + add;
+  } else {
+    
+  }
+
+  add = add + 1;
+}
+
+console.log(`Cумма нечетных чисел =  ${sum}`);
