89c59c570d864743575c4501671bc4e6eedeabd3
anbogod <pulseczar99@gmail.com>
2026-02-27 17:38:18 +0500

    complete lesson-05-01

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,13 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+      if (this.resources[resource] === undefined) {
+        console.log("Invalid resource");
+      } else {
+        this.resources[resource] += amount;
+      }  
+  }
 }
+
+
